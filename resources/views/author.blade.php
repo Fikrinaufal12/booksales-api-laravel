34a9a67fb72df9e41cefc25806ba3327eeed9b32
Fikrinaufal12 <136666261+Fikrinaufal12@users.noreply.github.com>
2025-05-22 13:54:17 +0700

    Tugas 3 Laravel

--- resources/views/author.blade.php
+++
@@ -1,6 +0,0 @@
-<h2>Daftar Author</h2>
-<ul>
-    @foreach($authors as $author)
-        <li>{{ $author['name'] }}</li>
-    @endforeach
-</ul>
--- resources/views/genre.blade.php
+++
@@ -1,6 +0,0 @@
-<h2>Daftar Genre</h2>
-<ul>
-    @foreach($genres as $genre)
-        <li>{{ $genre['name'] }}</li>
-    @endforeach
-</ul>
